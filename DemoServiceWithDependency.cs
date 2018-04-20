eda8bd5bc04647a309b819f29db3e291662463ba
ivupi <ivan.upirov@gmail.com>
2018-04-20 13:23:19 +0300

    Added DemoServiceWithDependency as a separate file

---
+++ DemoServiceWithDependency.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace UnitTestingDemoApplication.Tests
+{
+    public class DemoServiceWithDependency
+    {
+        private readonly IList _iList;
+
+        public DemoServiceWithDependency(IList iList)
+        {
+            _iList = iList;
+        }
+
+        public int Add(object content)
+        {
+            return _iList.Add(content);
+        }
+
+        public void AddRange(IList content)
+        {
+            foreach (var item in content)
+            {
+                _iList.Add(item);
+            }
+        }
+
+        public bool Contains(object subString)
+        {
+            return _iList.Contains(subString);
+        }
+
+        public int GetLength()
+        {
+            return _iList.Count;
+        }
+    }
+}
